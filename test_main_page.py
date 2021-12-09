c193aabba333fdc464014a59dec62359fd8f2faa
George <greeesha@gmail.com>
2021-12-09 21:39:03 +0300

    PageObject, 1st release, test login link

---
+++ test_main_page.py
@@ -0,0 +1,7 @@
+import pages.main_page
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = pages.main_page.MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
